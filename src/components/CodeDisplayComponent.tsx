545cd30eddb1ce2fcf68d3beb58e6b69deb0cf42
Ali <96635567+Touil-Ali@users.noreply.github.com>
2023-12-31 02:34:24 +0100

    implemnting coding visual using react-code-blocks

--- src/components/CodeDisplayComponent.tsx
+++ src/components/CodeDisplayComponent.tsx
@@ -1,48 +1,123 @@
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
+import { CopyBlock, dracula } from "react-code-blocks";
 import { JsxElement } from 'typescript';
+type NodeData = {
+  id: string;
+  data: {
+    label: string; attributes?: { name: string; type: string; visibility: string }[] | undefined;
+    methods?: { name: string; type: string; visibility: string }[] | undefined;
+  }
+  position: { x: number; y: number };
+  type?: string;
+  isClicked?: boolean;
+};
 
-
-interface CodeDisplayComponentProps { onClose: () => void;
+interface CodeDisplayComponentProps {
+  node: NodeData;
+  onClose: () => void;
+  code: string;
+  language: string;
+  showLineNumbers: boolean;
 }
-const CodeDisplayComponent: React.FC<CodeDisplayComponentProps> = ({ onClose  }) => { 
-  const [isOpen,setIsOpen] = useState(true);
-      // if(!onClose){
-      //   setIsOpen(false);
-     // }
-  const closeModel = () => {
-      setIsOpen(false);
-      // onclose();
-    }; 
-  
-return ( 
-<div>
-     
-{isOpen && (
-  <>
+const CodeDisplayComponent: React.FC<CodeDisplayComponentProps> = ({ node, onClose, code, language, showLineNumbers }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [theme, setTheme] = useState(dracula);
 
- <Popup open={true} modal>
-    <div className="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 md:p-8">
-    <span> Modal content </span>
-    <button
-            className="py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 sm:w-auto hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-            onClick={() => {
-              console.log('modal closed ');
-              setIsOpen(false);
-            }}
-          >
-            close modal
-          </button>
-          
-</div>
-  </Popup>
-  </>
+  const themes = [{ label: 'A11y Dark', value: 'a11yDark' },
+  { label: 'A11y Light', value: 'a11yLight' },
+  { label: 'An Old Hope', value: 'anOldHope' },
+  { label: 'Androidstudio', value: 'androidstudio' },
+  { label: 'Arta', value: 'arta' },
+  { label: 'Atom One Dark', value: 'atomOneDark' },
+  { label: 'Atom One Light', value: 'atomOneLight' },
+  { label: 'Codepen', value: 'codepen' },
+  { label: 'Dracula', value: 'dracula' },
+  { label: 'Far', value: 'far' },
+  { label: 'Github', value: 'github' },
+  { label: 'Googlecode', value: 'googlecode' },
+  { label: 'Hopscotch', value: 'hopscotch' },
+  { label: 'Hybrid', value: 'hybrid' },
+  { label: 'Ir Black', value: 'irBlack' },
+  { label: 'Mono Blue', value: 'monoBlue' },
+  { label: 'Monokai Sublime', value: 'monokaiSublime' },
+  { label: 'Monokai', value: 'monokai' },
+  { label: 'Nord', value: 'nord' },
+  { label: 'Obsidian', value: 'obsidian' },
+  { label: 'Noctis Viola', value: 'noctisViola' },
+  { label: 'Ocean', value: 'ocean' },
+  { label: 'Paraiso Dark', value: 'paraisoDark' },
+  { label: 'Paraiso Light', value: 'paraisoLight' },
+  { label: 'Pojoaque', value: 'pojoaque' },
+  { label: 'Purebasic', value: 'purebasic' },
+  { label: 'Railscast', value: 'railscast' },
+  { label: 'Rainbow', value: 'rainbow' },
+  { label: 'Shades Of Purple', value: 'shadesOfPurple' },
+  { label: 'Solarized Dark', value: 'solarizedDark' },
+  { label: 'Solarized Light', value: 'solarizedLight' },
+  { label: 'Sunburst', value: 'sunburst' },
+  { label: 'Tomorrow Night Blue', value: 'tomorrowNightBlue' },
+  { label: 'Tomorrow Night Bright', value: 'tomorrowNightBright' },
+  { label: 'Tomorrow Night Eighties', value: 'tomorrowNightEighties' },
+  { label: 'Tomorrow Night', value: 'tomorrowNight' },
+  { label: 'Tomorrow', value: 'tomorrow' },
+  { label: 'Vs2015', value: 'vs2015' },
+  { label: 'Xt256', value: 'xt256' },
+  { label: 'Zenburn', value: 'zenburn' },
+  ];
+  const handelChangeTheme = (e: any) => {
+    const theme = e.target.value;
+    console.log('theme', theme)
+    setTheme(theme);
+  };
+  return (
+    <div>
+
+      {isOpen && (
+        <>
+
+          <Popup open={true} modal>
+            <div className="relative p-4 w-auto bg-white rounded-lg shadow md:p-8 content-center">
+              <div className="flex justify-between items-center">
+                <span className="font-bold uppercase"> {node.data.label}</span>
+                <button
+                  className="py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 sm:w-auto hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                  onClick={() => {
+                    console.log('modal closed ');
+                    setIsOpen(false);
+                  }}
+                >
+                  close modal
+                </button>
 
 
- )}
+              </div>
+              <CopyBlock
+                text={code} language={language} showLineNumbers={showLineNumbers}
+                theme={theme}
+              />
+              <div>
 
- </div>
-);
+
+                <span>Select Theme:</span>
+                <div>
+                  <select value={theme.textColor} onChange={handelChangeTheme}>
+                    {themes.map((theme) => (
+                      <option key={theme.value} value={theme.value} > {theme.label}</option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            </div>
+          </Popup>
+        </>
+
+
+      )
+      }
+
+    </div >
+  );
 
 };
 export default CodeDisplayComponent;
--- src/pages/Factory.tsx
+++ src/pages/Factory.tsx
@@ -1,126 +1,132 @@
-import React, {useCallback, useState} from 'react';
+import React, { useCallback, useState } from 'react';
 import ReactFlow, {
-    addEdge,
-    Background,
-    BackgroundVariant, Connection,
-    Controls,
-    Edge,
-    MiniMap,
-    useEdgesState,
-    useNodesState
+  addEdge,
+  Background,
+  BackgroundVariant, Connection,
+  Controls,
+  Edge,
+  MiniMap,
+  useEdgesState,
+  useNodesState
 } from "reactflow";
-import UmlNode  from "../components/UmlNode";
 import 'reactflow/dist/style.css';
 import CodeDisplayComponent from "../components/CodeDisplayComponent";
-const UmlTables = [ {title: 'Circle', attributes: [{name: 'radius', type: 'int', visibility: 'private'}]}];
+import UmlNode from "../components/UmlNode";
 const nodeTypes = {
-    uml: UmlNode,
+  uml: UmlNode,
 };
-
-type NodeData = {
-    id: string;
-    data: { label: string;
-     attributes?: { name: string; type: string; visibility: string }[] | undefined ;
-        methods?: { name: string; type: string; visibility: string }[] | undefined;}
-    position: { x: number; y: number };
-    type?: string;
-    isClicked?: boolean;
-};
-const initialNodes:NodeData[] = [
-    {
-        id: '1',
-        data: {label: 'Circle'},
-        position: { x: 400, y: 300 },
-    },
-    {
-        id: '2',
-        data: {label: 'Square'},
-        position: { x: 800, y: 300 },
-    },
-    {
-        id: '3',
-        data: {label: 'Rectangle'},
-        position: { x: 600, y: 300 },
-    },
-    {
-        id: '4',
-        data: {label: 'Shape'},
-        position: { x: 600, y: 100 },
-        type: 'input',
-    },
-    {
-        id: '5',
-        data: {
-            label: 'exemple',
-            attributes: [{name: 'name', type: 'String', visibility: '+'},
-                {name: 'name', type: 'String', visibility: '+'}], // add attributes field
-            methods: [{name: 'name', type: 'String', visibility: '+'}],
-        },
-         position: {x: 600, y: 500},
-        type: 'uml',
+let javaCode = `
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
+}
+`;
+type NodeData = {
+  id: string;
+  data: {
+    label: string;
+    attributes?: { name: string; type: string; visibility: string }[] | undefined;
+    methods?: { name: string; type: string; visibility: string }[] | undefined;
+  }
+  position: { x: number; y: number };
+  type?: string;
+  isClicked?: boolean;
+};
+const initialNodes: NodeData[] = [
+  {
+    id: '1',
+    data: { label: 'Circle' },
+    position: { x: 400, y: 300 },
+  },
+  {
+    id: '2',
+    data: { label: 'Square' },
+    position: { x: 800, y: 300 },
+  },
+  {
+    id: '3',
+    data: { label: 'Rectangle' },
+    position: { x: 600, y: 300 },
+  },
+  {
+    id: '4',
+    data: { label: 'Shape' },
+    position: { x: 600, y: 100 },
+    type: 'input',
+  },
+  {
+    id: '5',
+    data: {
+      label: 'exemple',
+      attributes: [{ name: 'name', type: 'String', visibility: '+' },
+      { name: 'name', type: 'String', visibility: '+' }], // add attributes field
+      methods: [{ name: 'name', type: 'String', visibility: '+' }],
+    },
+    position: { x: 600, y: 500 },
+    type: 'uml',
+  }
 ];
 const initialEdges = [
 
-    {
-        id: 'e4-1',
-        source: '4',
-        target: '1',
-        label: 'implementes',
-        animated: true,
-    },
-    {
-        id: 'e4-2',
-        source: '4',
-        target: '2',
-        label: 'implementes',
-        animated: true,
-    },
-    {
-        id: 'e4-3',
-        source: '4',
-        target: '3',
-        label: 'implementes',
-        animated: true,
-    },
+  {
+    id: 'e4-1',
+    source: '4',
+    target: '1',
+    label: 'implementes',
+    animated: true,
+  },
+  {
+    id: 'e4-2',
+    source: '4',
+    target: '2',
+    label: 'implementes',
+    animated: true,
+  },
+  {
+    id: 'e4-3',
+    source: '4',
+    target: '3',
+    label: 'implementes',
+    animated: true,
+  },
 ];
 const Factory = () => {
 
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    const [clickedNode, setClickedNode] = useState<NodeData | null>(null); // Add this line
-    const [isOpen,setIsOpen] = useState(true);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [clickedNode, setClickedNode] = useState<NodeData | null>(null); // Add this line
 
-    const handleNodeClick = (event: React.MouseEvent, element: NodeData) => {
-                    if (element.type === 'uml') {
-                        setClickedNode((prev) => (prev?.id === element.id ? null : element));
-                    }
-                } 
-    const onConnect = useCallback(
-        (params: Edge<any> | Connection) => setEdges((eds) => addEdge(params, eds)),
-        [setEdges],
-    );
-    return (
-        <div  style={{ height: 600 }} >
-            <ReactFlow
-                nodes={nodes} edges={edges}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}
-                nodeTypes={nodeTypes}
-                onNodeClick={handleNodeClick}
+  const handleNodeClick = (event: React.MouseEvent, element: NodeData) => {
+    if (element.type === 'uml') {
+      setClickedNode((prev) => (prev?.id === element.id ? null : element));
+    }
+  }
+  const onConnect = useCallback(
+    (params: Edge<any> | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+  return (
+    <div style={{ height: 600 }} >
+      <ReactFlow
+        nodes={nodes} edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        onNodeClick={handleNodeClick}
 
-            >
-                <Controls />
-                <MiniMap />
-                <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-            </ReactFlow>
-            {clickedNode && (
-              <CodeDisplayComponent  onClose={ () => setClickedNode(null)} />
-            )}  
-      
-        </div>
-    );
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
+      {clickedNode && (
+        <CodeDisplayComponent node={clickedNode} onClose={() => setClickedNode(null)} code={javaCode} language='java' showLineNumbers={true} />
+      )}
+
+    </div>
+  );
 };
 
 export default Factory;
