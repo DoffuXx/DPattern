ad50156f272addb112f17c0fa20cd93b2a2ab336
DoffuXx <touilali2000@gmail.com>
2024-06-16 23:38:32 +0100

    add scroll react radiux

--- src/pages/app/layout/sideMenu.tsx
+++ src/pages/app/layout/sideMenu.tsx
@@ -1,4 +1,5 @@
 import navLinks from "@/registry/app/navLinks";
+import * as ScrollArea from "@radix-ui/react-scroll-area";
 import { useState } from "react";
 import { Outlet, NavLink } from "react-router-dom";
 
@@ -12,31 +13,51 @@ const SideMenu = () => {
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
           <ul className="space-y-2 font-medium">
-            {navLinks.map((link) => (
-              <li key={link.title}>
-                <p className="text-sm font-medium text-muted-foreground px-4 pb-2 max-w-[248px] truncate">
-                  {link.title}
-                </p>
-                {link.subLinks && (
-                  <ul id={`dropdown-${link.title}`} className="space-y-2 mt-1">
-                    {link.subLinks.map((subLink) => (
-                      <li key={subLink.title}>
-                        <NavLink
-                          to={subLink.path}
-                          className="flex items-center w-full p-2 text-gray-900 transition text-sm duration-75 pl-6 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                          style={({ isActive }) => ({
-                            fontWeight: isActive ? "bold" : "",
-                          })}
-                        >
-                          <subLink.icon className="w-5 h-5 me-2" />
-                          {subLink.title}
-                        </NavLink>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ))}
+            <ScrollArea.Root className="ScrollAreaRoot">
+              <ScrollArea.Viewport className="ScrollAreaViewport">
+                {navLinks.map((link) => (
+                  <li key={link.title}>
+                    <p className="text-sm font-medium text-muted-foreground px-4 pb-2 max-w-[248px] truncate">
+                      {link.title}
+                    </p>
+                    {link.subLinks && (
+                      <ul
+                        id={`dropdown-${link.title}`}
+                        className="space-y-2 mt-1"
+                      >
+                        {link.subLinks.map((subLink) => (
+                          <li key={subLink.title}>
+                            <NavLink
+                              to={subLink.path}
+                              className="flex items-center w-full p-2 text-gray-900 transition text-sm duration-75 pl-6 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                              style={({ isActive }) => ({
+                                fontWeight: isActive ? "bold" : "",
+                              })}
+                            >
+                              <subLink.icon className="w-5 h-5 me-2" />
+                              {subLink.title}
+                            </NavLink>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ))}
+              </ScrollArea.Viewport>
+              <ScrollArea.Scrollbar
+                className="ScrollAreaScrollbar"
+                orientation="vertical"
+              >
+                <ScrollArea.Thumb className="ScrollAreaThumb" />
+              </ScrollArea.Scrollbar>
+              <ScrollArea.Scrollbar
+                className="ScrollAreaScrollbar"
+                orientation="horizontal"
+              >
+                <ScrollArea.Thumb className="ScrollAreaThumb" />
+              </ScrollArea.Scrollbar>
+              <ScrollArea.Corner className="ScrollAreaCorner" />
+            </ScrollArea.Root>
           </ul>
         </div>
       </aside>
